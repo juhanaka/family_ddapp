4a5ed60f9597211f1f8d83c80b102c2e499bab51
ThomasPalomares <thopalomares@gmail.com>
2015-05-29 01:40:32 -0700

    chunking first trial

--- chunking/udf/ext_pronoun.py
+++ chunking/udf/ext_pronoun.py
@@ -5,17 +5,17 @@ import sys
 
 ARR_DELIM = '~^~'
 
+#Training set ? Supervision Rule ?
+
 # for each word
 for row in sys.stdin:
-	parts = row.strip().split('\t')
-	doc_id, s_words, s_sent_offset, s_sentence_id, p_sentence_id, p_start_position, p_length, p_text, p_mention_id, s_ner_tags = parts
-	
-	words = s_words.split(ARR_DELIM)
-  	ner_tags = s_ner_tags.split(ARR_DELIM)
+    parts = row.strip().split('\t')
+    doc_id, s_words, s_sent_offset, s_sentence_id, p_sentence_id, p_start_position, p_length, p_text, p_mention_id, s_ner_tags = parts
+    
+    words = s_words.split(ARR_DELIM)
+    ner_tags = s_ner_tags.split(ARR_DELIM)
 
-    #Training set ? Supervision Rule ?
-	if words[0].lower() == "he" or words[0].lower() == "she": 
-        is_true = '\N'
+    if words[0].lower() == "he" or words[0].lower() == "she":
         print '\t'.join(
             [ str(x) for x in [
             '%s_0' % s_sentence_id,
@@ -25,7 +25,7 @@ for row in sys.stdin:
             s_sentence_id,
             s_sent_offset,
             p_text,
-            is_true,
+            '\N',
             '%s_0-%s' % (s_sentence_id, p_mention_id), 
             doc_id,
             '\N'
@@ -34,17 +34,17 @@ for row in sys.stdin:
 	for index in range(1, len(words)):
 		if words[index].lower() == "he" or words[index].lower() == "she":
 			print '\t'.join(
-      		[ str(x) for x in [
-        	'%s_%d' % (s_sentence_id, index),
-        	p_mention_id,
-        	index,
- 			words[index],
-        	s_sentence_id,
-        	s_sent_offset,
-        	p_text,
-        	'\N',
-        	'%s_%d-%s' % (s_sentence_id, index, p_mention_id), 
-        	doc_id,
-        	'\N'
-      		]])
+      		    [ str(x) for x in [
+                	'%s_%d' % (s_sentence_id, index),
+                	p_mention_id,
+                	index,
+         			words[index],
+                	s_sentence_id,
+                	s_sent_offset,
+                	p_text,
+                	'\N',
+                	'%s_%d-%s' % (s_sentence_id, index, p_mention_id), 
+                	doc_id,
+                	'\N'
+      		    ]])
 
