d9b4f53216b780d148967f7c81ac5b467dc4f30b
ThomasPalomares <thopalomares@gmail.com>
2015-06-03 22:15:20 -0700

    improvement for recall and all the tags exported so far

--- estimate_precision/estimate_precision_parent.sh
+++ estimate_precision/estimate_precision_parent.sh
@@ -2,6 +2,7 @@
 
 DBNAME=deepdive_family
 file_name=tags.sql
+file_name_recall=tags_recall.sql
 
 if psql -lqt | cut -d \| -f 1 | grep -w $DBNAME; then
     echo "database " $DBNAME " already exists, adding new value"
@@ -12,13 +13,16 @@ fi
 
 psql -d $DBNAME -c "drop table if exists permanent_tags_family_parent_precision_is_correct;"
 
-psql -d $DBNAME < permanent_tags_family_parent_precision_is_correct.sql
+if [ -f "permanent_tags_family_parent_precision_is_correct.sql" ]
+  then
+    psql -d $DBNAME < permanent_tags_family_parent_precision_is_correct.sql
+fi
 
 if [ -f "$file_name" ]
   then
-    perl -pi -e 's/tags_parent-precision_is_correct/tags_parent_precision_is_correct/' tags.sql
+    perl -pi -e 's/tags_parent-precision_is_correct/tags_parent_precision_is_correct/' $file_name
 
-    psql $DBNAME < tags.sql
+    psql $DBNAME < $file_name
 
     psql -d $DBNAME -c \
     """
@@ -47,6 +51,45 @@ if [ -f "$file_name" ]
            t.is_correct,
            t.description
     FROM tags_parent_precision_is_correct t;
+    """
+fi
+
+
+if [ -f "$file_name_recall" ]
+  then
+    perl -pi -e 's/tags_parent-recall_is_correct/tags_parent_recall_is_correct/' $file_name_recall
+
+    psql $DBNAME < $file_name_recall
+
+    psql -d $DBNAME -c \
+    """
+    CREATE TABLE IF NOT EXISTS permanent_tags_family_parent_precision_is_correct
+    ( relation_id TEXT, 
+      sentence_id TEXT, 
+      is_correct TEXT, 
+      description TEXT,
+      expectation double precision
+    );
+    """
+
+    psql -d $DBNAME -c \
+    """
+    DELETE FROM permanent_tags_family_parent_precision_is_correct
+    where sentence_id IN 
+    (SELECT tags_parent_recall_is_correct.sentence_id FROM tags_parent_recall_is_correct 
+    where permanent_tags_family_parent_precision_is_correct.description = tags_parent_recall_is_correct.description
+    and tags_parent_recall_is_correct.is_correct != '');
+    """
+
+    psql -d $DBNAME -c \
+    """
+    INSERT INTO permanent_tags_family_parent_precision_is_correct
+    SELECT t.relation_id, 
+           t.sentence_id, 
+           'true',
+           t.description
+    FROM tags_parent_recall_is_correct t
+    WHERE t.is_correct != '';
     """
 fi
 
