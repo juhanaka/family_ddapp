29da65b3507c204879d4679ac5259b0d4cfdefb1
ThomasPalomares <thopalomares@gmail.com>
2015-06-04 17:46:43 -0700

    improvment chunking

--- chunking/udf/ext_pronoun.py
+++ chunking/udf/ext_pronoun.py
@@ -10,26 +10,41 @@ ARR_DELIM = '~^~'
 # for each word
 for row in sys.stdin:
     parts = row.strip().split('\t')
-    doc_id, s_words, s_sent_offset, s_sentence_id, p_sentence_id, p_start_position, p_length, p_text, p_mention_id, s_people_ner_tags = parts
+    doc_id, s_sent_words, s_sent_offset, s_sentence_id, p_sentence_id, p_start_position, p_length, p_text, p_mention_id, s_people_ner_tags, s_people_words, s_sent_ner_tags = parts
     
-    words = s_words.split(ARR_DELIM)
-    ner_tags = s_people_ner_tags.split(ARR_DELIM)
+    s_sent_words = s_sent_words.split(ARR_DELIM)
+    s_people_words = s_people_words.split(ARR_DELIM)
+    s_people_ner_tags = s_people_ner_tags.split(ARR_DELIM)
+    s_sent_ner_tags = s_sent_ner_tags.split(ARR_DELIM)
+    p_start_position = int(p_start_position)
+    p_length = int(p_length)
     first_pronoun=0
     phrases = []
     is_true='\N'
-    unique_people_mention = True
+    potential_true_training_set = True
 
-    for i in range (0, len(ner_tags)):
+    words_not_true_training_data = ["he", "she", "his", "her"]
+
+    for i in range (0, len(s_people_ner_tags)):
         if i < p_start_position or i >= (p_start_position + p_length):
-            if ner_tags[i]== "PERSON":
-                unique_people_mention = False
+            if s_people_ner_tags[i]== "PERSON" or s_people_words[i].lower() in words_not_true_training_data:
+                potential_true_training_set = False
+                #print >> sys.stderr, "False", s_people_ner_tags[i], i
 
-    for index in range(0, len(words)):
-        if words[index].lower() == "he" or words[index].lower() == "she":
+    for index in range(0, len(s_sent_words)):
+        if s_sent_words[index].lower() == "he" or s_sent_words[index].lower() == "she":
             first_pronoun +=1
             phrases.append(index)
+        if s_sent_ner_tags[index] == "PERSON":
+            potential_true_training_set = False
 
-    if first_pronoun==1 and unique_people_mention==True:
+    if first_pronoun != 1:
+        potential_true_training_set = False
+
+    #print >> sys.stderr, s_sentence_id, first_pronoun, potential_true_training_set, p_start_position, p_length
+
+
+    if potential_true_training_set==True:
         is_true='1'
 
     for index in phrases:
@@ -38,7 +53,7 @@ for row in sys.stdin:
                 '%s_%d' % (s_sentence_id, index),
                 p_mention_id,
                 index,
-                words[index],
+                s_sent_words[index],
                 s_sentence_id,
                 s_sent_offset,
                 p_text,
