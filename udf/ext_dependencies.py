c2b25481310c16e7bf3e3caed74daa551ef34bed
ThomasPalomares <thopalomares@gmail.com>
2016-02-21 17:17:54 -0800

    attempt to make it run - still an issue with ddlib

--- udf/ext_dependencies.py
+++ udf/ext_dependencies.py
@@ -11,40 +11,28 @@ from deepdive import *
  pos_tags         =  "text[]",  
  dependencies     =  "text[]",  
  ner_tags         =  "text[]",  
- sentence_offset  =  "bigint",  
+ sentence_offset  =  "text",  
  sentence_id      =  "text",    
   :[])
-def extract(
+def ext_dependencies(
  document_id          =  "text",       
  sentence             =  "text",       
  words                =  "text[]",     
  lemma                =  "text[]",     
  pos_tags             =  "text[]",     
  dependency_labels    =  "text[]",     
- dependency_parents   =  "integer[]",  
+ dependency_parents   =  "int[]",  
  ner_tags             =  "text[]",     
- sentence_offset      =  "bigint",     
+ sentence_offset      =  "text",     
  sentence_id          =  "text",
   ):
   dependency_strings = []
   try:
     for i, label in enumerate(dependency_labels):
         parent_idx = int(dependency_parents[i])
-        dependency_string = '{0}({1}-{2}, {3}-{4})'.format(label,
-                                                           words[parent_idx-1],
-                                                           parent_idx,
-                                                           words[i],
-                                                           i+1)
+        dependency_string = '{0}({1}-{2}, {3}-{4})'.format(label, words[parent_idx-1],parent_idx,words[i],i+1)
         dependency_strings.append(dependency_string)
   except IndexError:
       dependency_strings = None
 
-  yield [  document_id,
-           sentence,
-           words,
-           lemma,
-           pos_tags,
-           dependency_strings,
-           ner_tags,
-           sentence_offset,
-           sentence_id ]
+  yield [document_id,sentence,words,lemma,pos_tags,dependency_strings,ner_tags,sentence_offset,sentence_id ]
